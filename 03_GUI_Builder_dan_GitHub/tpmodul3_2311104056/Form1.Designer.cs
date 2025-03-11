8ad30b6d4e3abcd4adb281c60a0e85f46f2a3930
hazazaidan <fannzidna3@gmail.com>
2025-03-11 13:05:42 +0700

    menambahkan gui

---
+++ 03_GUI_Builder_dan_GitHub/tpmodul3_2311104056/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace tpmodul3_2311104056
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNama = new System.Windows.Forms.TextBox();
+            this.buttonGetNama = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxNama
+            // 
+            this.textBoxNama.Location = new System.Drawing.Point(275, 215);
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(100, 26);
+            this.textBoxNama.TabIndex = 0;
+            // 
+            // buttonGetNama
+            // 
+            this.buttonGetNama.Location = new System.Drawing.Point(391, 217);
+            this.buttonGetNama.Name = "buttonGetNama";
+            this.buttonGetNama.Size = new System.Drawing.Size(99, 44);
+            this.buttonGetNama.TabIndex = 1;
+            this.buttonGetNama.Text = "GetNam";
+            this.buttonGetNama.UseVisualStyleBackColor = true;
+            this.buttonGetNama.Click += new System.EventHandler(this.buttonGetNama_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonGetNama);
+            this.Controls.Add(this.textBoxNama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNama;
+        private System.Windows.Forms.Button buttonGetNama;
+    }
+}
+
