306a9c64ade869252f1ec1f93a43d3add53c01e3
hazazaidan <fannzidna3@gmail.com>
2025-03-11 13:18:45 +0700

    Hallo

---
+++ 05_Generics/Tpmodul5_2311104056/Hallo.cs
@@ -0,0 +1,18 @@
+using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("NamaPanggilan"); // Ganti dengan nama praktikan
+    }
+}
