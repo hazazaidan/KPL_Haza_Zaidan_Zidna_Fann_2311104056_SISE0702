7d449966d22bad4a720d4f3159d7a4c0445e3e03
hazazaidan <fannzidna3@gmail.com>
2025-03-11 13:21:39 +0700

    genericclass

--- 05_Generics/Tpmodul5_2311104056/Hallo.cs
+++ 05_Generics/Tpmodul5_2311104056/Hallo.cs
@@ -16,3 +16,27 @@ class Program
         halo.SapaUser("NamaPanggilan"); // Ganti dengan nama praktikan
     }
 }
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataGeneric<string> data = new DataGeneric<string>("2311104056"); // Ganti dengan NIM
+        data.PrintData();
+    }
+}
