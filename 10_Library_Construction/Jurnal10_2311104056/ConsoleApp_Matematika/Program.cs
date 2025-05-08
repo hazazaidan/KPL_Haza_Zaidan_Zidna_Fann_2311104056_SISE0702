a8d34bfde0914e455e8409076cefe964e5b5e288
hazazaidan <fannzidna3@gmail.com>
2025-05-08 14:23:20 +0700

    menambahkan library MatematikaLibraries

---
+++ 10_Library_Construction/Jurnal10_2311104056/ConsoleApp_Matematika/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MatematikaLibraries;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("FPB dari 60 dan 45: " + MathHelper.FPB(60, 45));         // Output: 15
+        Console.WriteLine("KPK dari 12 dan 8: " + MathHelper.KPK(12, 8));           // Output: 24
+
+        int[] persamaan1 = { 1, 4, -12, 9 };
+        Console.WriteLine("Turunan dari x^3 + 4x^2 - 12x + 9: " + MathHelper.Turunan(persamaan1));
+        // Output: 3x2 +8x -12
+
+        int[] persamaan2 = { 4, 6, -12, 9 };
+        Console.WriteLine("Integral dari 4x^3 + 6x^2 - 12x + 9: " + MathHelper.Integral(persamaan2));
+        // Output: x4 +2x3 -6x2 +9x + C
+    }
+}
---
+++ 10_Library_Construction/Jurnal10_2311104056/MatematikaLibraries/MathHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatematikaLibraries
+{
+    public class MathHelper
+    {
+        public static int FPB(int input1, int input2)
+        {
+            while (input2 != 0)
+            {
+                int temp = input2;
+                input2 = input1 % input2;
+                input1 = temp;
+            }
+            return input1;
+        }
+
+        public static int KPK(int input1, int input2)
+        {
+            return (input1 * input2) / FPB(input1, input2);
+        }
+
+        public static string Turunan(int[] persamaan)
+        {
+            List<string> hasil = new List<string>();
+            int pangkat = persamaan.Length - 1;
+
+            for (int i = 0; i < persamaan.Length - 1; i++)
+            {
+                int koef = persamaan[i] * (pangkat - i);
+                int derajat = pangkat - i - 1;
+
+                if (koef == 0) continue;
+
+                string term = "";
+
+                if (koef > 0 && hasil.Count > 0)
+                    term += "+";
+
+                if (derajat == 0)
+                    term += $"{koef}";
+                else if (derajat == 1)
+                    term += $"{koef}x";
+                else
+                    term += $"{koef}x{derajat}";
+
+                hasil.Add(term);
+            }
+
+            return string.Join(" ", hasil);
+        }
+
+        public static string Integral(int[] persamaan)
+        {
+            List<string> hasil = new List<string>();
+            int pangkat = persamaan.Length;
+
+            for (int i = 0; i < persamaan.Length; i++)
+            {
+                double koef = (double)persamaan[i] / (pangkat - i);
+                int derajat = pangkat - i;
+
+                string strKoef = koef == 1 ? "" :
+                                 koef == -1 ? "-" :
+                                 koef.ToString("G");
+
+                string term = "";
+
+                if (koef > 0 && hasil.Count > 0)
+                    term += "+";
+
+                if (derajat == 1)
+                    term += $"{strKoef}x";
+                else
+                    term += $"{strKoef}x{derajat}";
+
+                hasil.Add(term);
+            }
+
+            hasil.Add("+ C");
+            return string.Join(" ", hasil);
+        }
+    }
+}
