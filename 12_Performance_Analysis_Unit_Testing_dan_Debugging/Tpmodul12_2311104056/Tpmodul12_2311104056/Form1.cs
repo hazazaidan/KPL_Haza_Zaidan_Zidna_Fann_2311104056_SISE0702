abad0cd8b3822135d6b54f0514b18cf0d67d9459
hazazaidan <fannzidna3@gmail.com>
2025-05-08 15:28:50 +0700

    Upload Tp Modul12

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/Tpmodul12_2311104056/Tpmodul12_2311104056/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tpmodul12_2311104056
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Mengambil input dari textBox1
+            int input;
+            // Mengecek apakah input bisa dikonversi menjadi integer
+            if (int.TryParse(textBox1.Text, out input))
+            {
+                // Menampilkan hasil pengecekan di label1
+                label1.Text = CariTandaBilangan(input);
+            }
+            else
+            {
+                // Menampilkan pesan jika input tidak valid
+                label1.Text = "Input tidak valid";
+            
+        }
+        }
+        public string CariTandaBilangan(int a)
+        {
+            if (a < 0)
+                return "Negatif";
+            else if (a > 0)
+                return "Positif";
+            else
+                return "Nol";
+        }
+    }
+}
