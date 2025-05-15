58aca13eb6e3d95a3a1a21eef16e2482b9ca9809
hazazaidan <fannzidna3@gmail.com>
2025-05-15 17:29:37 +0700

    Menambahkan Jurnal Modul 12, file builder pattern, dan perubahan Form1.cs

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/Jurnal_Modul12_2311104056/Jurnal_Modul12_2311104056/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jurnal_Modul12_2311104056
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int CariNilaiPangkat(int a, int b)
+        {
+            if (b == 0)
+                return 1;
+            if (b < 0)
+                return -1;
+            if (b > 10 || a > 100)
+                return -2;
+
+            try
+            {
+                checked
+                {
+                    int hasil = 1;
+                    for (int i = 0; i < b; i++)
+                    {
+                        hasil *= a;
+                    }
+                    return hasil;
+                }
+            }
+            catch (OverflowException)
+            {
+                return -3;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int a) && int.TryParse(textBox2.Text, out int b))
+            {
+                int hasil = CariNilaiPangkat(a, b);
+                label2.Text = $"Hasil: {hasil}";
+            }
+            else
+            {
+                label2.Text = "Input tidak valid!";
+            }
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
