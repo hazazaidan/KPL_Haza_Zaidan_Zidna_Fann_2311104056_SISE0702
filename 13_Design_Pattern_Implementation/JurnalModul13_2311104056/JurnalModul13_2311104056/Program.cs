73a8e82d12e393ba49059b6ee056506e87568b74
hazazaidan <fannzidna3@gmail.com>
2025-05-22 13:39:35 +0700

    UploadJurnal13

---
+++ 13_Design_Pattern_Implementation/JurnalModul13_2311104056/JurnalModul13_2311104056/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var data1 = PusatDataSingleton.GetDataSingleton();
+        var data2 = PusatDataSingleton.GetDataSingleton();
+
+        // Tambahkan data anggota kelompok dan asisten
+        data1.AddSebuahData("Jono");
+        data1.AddSebuahData("Jafar");
+        data1.AddSebuahData("Asisten: Budi");
+
+        Console.WriteLine("Data di data2 (setelah penambahan):");
+        data2.PrintSemuaData();
+
+        // Hapus asisten
+        data2.HapusSebuahData(2);
+
+        Console.WriteLine("\nData di data1 (setelah penghapusan asisten):");
+        data1.PrintSemuaData();
+
+        Console.WriteLine($"\nJumlah data di data1: {data1.GetSemuaData().Count}");
+        Console.WriteLine($"Jumlah data di data2: {data2.GetSemuaData().Count}");
+    }
+}
---
+++ 13_Design_Pattern_Implementation/JurnalModul13_2311104056/JurnalModul13_2311104056/PusatDataSingleton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PusatDataSingleton
+{
+    private static PusatDataSingleton _instance;
+    public List<string> DataTersimpan;
+
+    private PusatDataSingleton()
+    {
+        DataTersimpan = new List<string>();
+    }
+
+    public static PusatDataSingleton GetDataSingleton()
+    {
+        if (_instance == null)
+        {
+            _instance = new PusatDataSingleton();
+        }
+        return _instance;
+    }
+
+    public List<string> GetSemuaData()
+    {
+        return DataTersimpan;
+    }
+
+    public void PrintSemuaData()
+    {
+        foreach (var data in DataTersimpan)
+        {
+            Console.WriteLine(data);
+        }
+    }
+
+    public void AddSebuahData(string input)
+    {
+        DataTersimpan.Add(input);
+    }
+
+    public void HapusSebuahData(int index)
+    {
+        if (index >= 0 && index < DataTersimpan.Count)
+        {
+            DataTersimpan.RemoveAt(index);
+        }
+    }
+}
