0c19a6c1defaa22b4a13b3a0c7ad4580d9e810cf
hazazaidan <fannzidna3@gmail.com>
2025-05-22 13:58:31 +0700

    Refactor TPModul14 - Clean Code

---
+++ 14_Clean_Code/TPModul14_2311104056/TPModul14_2311104056/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        InputHuruf();
+        CetakBilanganGenap();
+    }
+
+    // Method untuk menerima input huruf dan menentukan jenis huruf
+    static void InputHuruf()
+    {
+        Console.Write("Masukkan satu huruf: ");
+        char inputChar = char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        if ("AIUEO".Contains(inputChar))
+        {
+            Console.WriteLine($"Huruf {inputChar} merupakan huruf vokal");
+        }
+        else
+        {
+            Console.WriteLine($"Huruf {inputChar} merupakan huruf konsonan");
+        }
+    }
+
+    // Method untuk mencetak array bilangan genap
+    static void CetakBilanganGenap()
+    {
+        int[] evenNumbers = { 2, 4, 6, 8, 10 };
+
+        Console.WriteLine("Daftar angka genap:");
+        for (int i = 0; i < evenNumbers.Length; i++)
+        {
+            Console.WriteLine($"Angka genap ke-{i + 1}: {evenNumbers[i]}");
+        }
+    }
+}
